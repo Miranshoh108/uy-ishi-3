f1b881bec707e58e5ce9c7c07c993707498d3d39
Muhammad <miranshoh108@gamil.com>
2024-12-04 00:04:53 +0500

    done

---
+++ main.js
@@ -0,0 +1,44 @@
+//1-masala
+// let son = 75;
+// if (son >= 50 && son <= 100) {
+//     console.log("Son 50 va 100 orasida.");
+// } else {
+//     console.log("Son 50 va 100 orasida emas.");
+// }
+//2-masala
+// let number = 5.7;
+// console.log("Math.floor:", Math.floor(number)); 
+// console.log("Math.ceil:", Math.ceil(number));  
+//3-masala
+// let randomSon = Math.floor(Math.random() * 100) + 1;
+// console.log("Tasodifiy son:", randomSon);
+// if (randomSon % 2 === 0) {
+//     console.log("Son juft.");
+// } else {
+//     console.log("Son toq.");
+// }
+//4-masala
+// let radius = 7;
+// let diameter = 2 * radius;
+// let uzunlik = 2 * Math.PI * radius;
+// console.log("Diametri:", diameter);
+// console.log("Uzunligi:", uzunlik.toFixed(2));
+//5-masala
+// let a = 12, b = 45, c = 7, d = 33;
+// let maxSon = Math.max(a, b, c, d);
+// console.log("Eng katta son:", maxSon);
+//6-masala
+// let x = 10, y = 5;
+// if (x > y) {
+//     console.log("Ko'paytma:", x * y);
+// } else {
+//     console.log("Bo'linma:", x / y);
+// }
+//7-masala
+// let son2 = 25;
+// let ildiz = Math.sqrt(son2);
+// if (ildiz === Math.floor(ildiz)) {
+//     console.log("Butun ildiz.");
+// } else {
+//     console.log("Butun emas.");
+// }
